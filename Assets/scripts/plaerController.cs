be09bf454cb0412bd066044243c1ea185df290a9
Alex235064 <116053389+Alex235064@users.noreply.github.com>
2024-02-07 21:27:02 +0700

    Movement

--- Assets/scripts/plaerController.cs
+++ Assets/scripts/plaerController.cs
@@ -6,8 +6,10 @@ public class plaerController : MonoBehaviour
 {
 public float Gravity = 9.8f;
     public float JampForce;
+    public float Speed;
 
     private float _fallVelocity = 1;
+    private Vector3 _moveVector;
 
     private CharacterController _CharacterController;
     
@@ -20,14 +22,33 @@ public float Gravity = 9.8f;
     // Update is called once per frame
     private void Update()
     {
-         if (Input.GetKeyDown(KeyCode.Space))
+
+        _moveVector = Vector3.zero;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            _moveVector += transform.forward;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            _moveVector -= transform.forward;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            _moveVector += transform.right;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            _moveVector -= transform.right;
+        }
+        if (Input.GetKeyDown(KeyCode.Space) && _CharacterController.isGrounded)
         {
           _fallVelocity = -JampForce;
         }
     }
     void FixedUpdate()
     {
-       
+        _CharacterController.Move(_moveVector * Speed * Time.fixedDeltaTime);
 
         _fallVelocity += Gravity * Time.fixedDeltaTime;
         _CharacterController.Move(Vector3.down * _fallVelocity * Time.fixedDeltaTime);
